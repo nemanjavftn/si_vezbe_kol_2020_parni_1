4b0610a4d89da084ebe411d237f7b18e25e638ae
student03 <student03@LENOVO03>
2020-12-03 17:56:56 +0100

    Kreirana tabela Products.

---
+++ DataBase/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(2000) NULL, 
+    [ExpiryDate] DATETIME NULL
+
+)
