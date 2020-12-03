817c2372eeb30173bdb53a876c829b7305ac3c99
Stefan Debeljak <stefan_debeljak@yahoo.com>
2020-12-03 18:17:59 +0100

    U business layeru kreirane metode za unos i dovlacenje proizvoda iz repozitorijuma. U data layeru kreiran repozitorijum kao i metode za unos i dovlacenje proizvoda iz baze. Na presentation layeru kreirano je dugme koje omogucava unos proizvoda u listbox i nakon unosa se refreshuju polja za unos.

---
+++ DataLayer/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class ProductRepository
+    {
+        public List<Product> GetAllProducts()
+        {
+            List<Product> results = new List<Product>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Product";
+
+                sqlConnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Product p = new Product();
+                    p.Id = sqlDataReader.GetInt32(0);
+                    p.Name = sqlDataReader.GetString(1);
+                    p.Description = sqlDataReader.GetString(2);
+                    p.ExpiryDate = sqlDataReader.GetString(3);
+
+                    results.Add(p);
+                }
+            }
+
+            return results;
+        }
+        public int InsertProduct(Product p)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                    string.Format("INSERT INTO Students VALUES ('{0}', '{1}', {2})",
+                        p.Name, p.Description, p.ExpiryDate);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- PresentationLayer/Form1.cs
+++ PresentationLayer/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer;
+using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,47 @@ namespace PresentationLayer
 {
     public partial class Form1 : Form
     {
+        private readonly ProductBusiness productBusiness;
         public Form1()
         {
             InitializeComponent();
+            this.productBusiness = new ProductBusiness();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void buttonInsertProduct_Click(object sender, EventArgs e)
+        {
+           Product p = new Product();
+            p.Name = textBoxName.Text;
+            p.Description = textBoxDescription.Text;
+            p.ExpiryDate = textBoxExpiryDate.Text;
+
+            if (this.productBusiness.InsertProduct(p))
+            {
+                RefreshData();
+                textBoxName.Text = "";
+                textBoxDescription.Text = "";
+                textBoxExpiryDate.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Greska!");
+            }
+        }
+        private void RefreshData()
+        {
+            List<Product> products = this.productBusiness.GetAllProducts();
+            listBoxProducts.Items.Clear();
+
+            foreach (Product p in products)
+            {
+                listBoxProducts.Items.Add(p.Id + ". " + p.Name + ", " + p.Description + ", " +
+                    p.ExpiryDate + ".");
+            }
         }
     }
 }
