17e82138eb3facc1c664f3f5de52a71accb5619a
Nikola <theracermx@gmail.com>
2020-12-03 18:00:41 +0100

    Kreirana troslojna arhitektura i baza podataka

---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(2000) NULL, 
+    [ExpiryDate] DATETIME NULL
+)
