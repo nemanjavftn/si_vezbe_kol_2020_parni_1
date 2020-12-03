9b8cc5dde70e79bd169bc2b360ed0ab55fa344d2
student03 <student03@LENOVO03>
2020-12-03 18:15:15 +0100

    Kreirana klasa Products.

---
+++ PresentationLayer/DataAccessLayer/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
